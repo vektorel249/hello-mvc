dd623d98868babf73c5da064f3e739f599a9fb26
vektorel249@outlook.com <vektorel249@outlook.com>
2025-04-05 10:16:49 +0300

    new project

---
+++ Vektorel.Web/Vektorel.GameCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vektorel.GameCenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
